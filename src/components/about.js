09cf96f3927d089046d7ce3ab359641122480caf
jiyuu-jin <jiyuu.llc@gmail.com>
2017-11-15 18:51:12 -0500

    More styling fixes

--- src/components/about.js
+++ src/components/about.js
@@ -7,7 +7,7 @@ const About = () =>
       <div className="card-content" />
       <div className="card-content">
         <h2 className="center">Who We Are</h2>
-        <h4 className="center">Internal Medicine Consultants has been serving the South Central Pennsylvania area for the past 40 years. Originally establish in 1977, we having been providing unmatched personal care for all our patient's medical concerns. Whether its same day appointments, to in-house lab work or preventive care, our focus is on the patients’ needs above all else. As a patient at IMC, we want you to feel confident in your health and know that your medical issues are heard and are being cared for. We offer a state-of-the-art facility that allows us to be your one-stop shop for your health care needs. In addition to medical care, we also offer the latest in medical aesthetics. Just another way that we here at IMC aim to have you feeling great and looking great.</h4>
+        <h5>Internal Medicine Consultants has been serving the South Central Pennsylvania area for the past 40 years. Originally establish in 1977, we having been providing unmatched personal care for all our patient's medical concerns. Whether its same day appointments, to in-house lab work or preventive care, our focus is on the patients’ needs above all else. As a patient at IMC, we want you to feel confident in your health and know that your medical issues are heard and are being cared for. We offer a state-of-the-art facility that allows us to be your one-stop shop for your health care needs. In addition to medical care, we also offer the latest in medical aesthetics. Just another way that we here at IMC aim to have you feeling great and looking great.</h5>
       </div>;
     </div>
     <br />
