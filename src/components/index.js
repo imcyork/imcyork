2a51a2424b14f039ce51e0581a43140b4f062518
jiyuu-jin <jiyuu.llc@gmail.com>
2018-01-21 22:07:04 -0500

    Navigation fixes

--- src/components/index.js
+++ src/components/index.js
@@ -8,8 +8,6 @@ const IndexPage = () => ({
         <br />
         <br />
         <br />
-        <br />
-        <br />
         <div className="top_options">
           <a href="https://www.medentmobile.com/portal/index.php?practice_id=8fTfL3cT" className="orange-text"> MEDENT Patient Portal</a>
           <a href="/images/form.pdf" className="orange-text">New Patient Form</a>
