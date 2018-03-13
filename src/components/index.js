d8f3921fc7e1063e65486e6ca1b268ba5e5d3d74
jiyuu-jin <jiyuu.llc@gmail.com>
2018-03-13 02:29:21 -0400

    Fixed broken links

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const IndexPage = () => ({
@@ -15,8 +16,8 @@ const IndexPage = () => ({
           <div className="card-content">
             <div className="carousel fix">
               <a className="carousel-item" href="https://www.google.com/maps/dir/''/1777+5th+Ave,+York,+PA+17403/data=!4m5!4m4!1m0!1m2!1m1!1s0x89c88ee09abdd845:0x647241f36cd01fa6?sa=X&ved=0ahUKEwixzvTnt5nWAhWFyyYKHZ2cAhIQwwUIKTAA"><img src="/images/imc.jpg" /></a>
-              <a className="carousel-item" href="/sculpsure"><img alt="" src="/images/sculpt.jpg" /></a>
-              <a className="carousel-item" href="/mdvip"><img alt="" src="/images/mdvip.jpg" /></a>
+              <Link className="carousel-item" to="/sculpsure"><img alt="" src="/images/sculpt.jpg" /></Link>
+              <Link className="carousel-item" to="/mdvip"><img alt="" src="/images/mdvip.jpg" /></Link>
             </div>
           </div>
         </div>
