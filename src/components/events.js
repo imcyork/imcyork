c8c4365742ddb202e74c6bedb6f7043f8aae3f2f
jiyuu-jin <jiyuu.llc@gmail.com>
2018-03-27 13:27:23 -0400

    Events page layout updates

--- src/components/events.js
+++ src/components/events.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/prefer-stateless-function,no-underscore-dangle */
 import React, { Component } from 'react';
 
 class Events extends Component {
@@ -12,26 +12,38 @@ class Events extends Component {
   componentDidMount() {
     const dataURL = 'https://blog.imcyork.com/wp-json/wp/v2/events?_embed';
     fetch(dataURL)
-            .then(res => res.json())
-            .then((res) => {
-              this.setState({
-                events: res,
-              });
-            });
+        .then(res => res.json())
+        .then((res) => {
+          this.setState({
+            events: res,
+          });
+        });
   }
 
-
   render() {
-    const events = this.state.events.map((event, index) => <div key={index}>
-      <p className="event-item">{event.acf.event_text}</p>
-    </div>);
+    const events = this.state.events.map((event, index) =>
+
+      <div key={index} className="row">
+        <div className="col s12 m4 offset-m4">
+          <div className="card">
+            <div className="card-image">
+              <img alt="Event" src={event._embedded['wp:featuredmedia']['0'].source_url} />
+            </div>
+            <div className="card-content">
+              <p>{event.acf.event_title}</p>
+            </div>
+          </div>
+        </div>
+      </div>,
+    );
+
 
     return (
       <div>
         <div className="spacer" />
         <div className="card special-card">
           <div className="card-content">
-            <h2 className="center ">Events/Specials</h2>
+            <h2 className="special-title">Events/Specials</h2>
             {events}
           </div>
         </div>
